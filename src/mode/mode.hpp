bc41906fdd4a724bd4d66b81bc6f3c1da92db747
Hoang Le <>
2024-11-15 09:55:36 +0200

    Add file for different modules

---
+++ src/mode/mode.hpp
@@ -0,0 +1,6 @@
+#ifndef MODE_HPP
+#define MODE_HPP
+
+// TODO: HoangLe [Nov-13]: Define 2 modes for the nodes: Master and Data node
+
+#endif
---
+++ src/transmission/packets.hpp
@@ -0,0 +1,9 @@
+#ifndef PACKETS_HPP
+#define PACKETS_HPP
+
+struct Packet
+{
+    // TODO: HoangLe [Nov-13]: Continue this
+};
+
+#endif
